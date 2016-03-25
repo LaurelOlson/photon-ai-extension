742ede88d4535599449667d6c42fab5c8982f639
Jedeu <jed.piezas@gmail.com>
2016-03-25 01:11:49 +0000

    Took care of _[a-z] issues and multiple _ issues

--- app/embeds/photon-flickr.js
+++ app/embeds/photon-flickr.js
@@ -27,15 +27,30 @@ $(function(){
         var cleanStyle = cleanUpStyle(styleAttrib);
     }
 
+    // Handles Flickr's weird way of embedding the img URL in the css code
     function createPhotoUrl(styleAttrib) {
         var cleanStyle = cleanUpStyle(styleAttrib);
         var photoUrl = cleanStyle['background-image'];
         var linkLinter = /\(([^\)]+)\)/;
-        var theLink = (((photoUrl.match(linkLinter)[1]).replace(/"/g, "")).replace(/.jpg/g, "").replace(/_.*/g, ""));
-        return theLink + '_b.jpg';
+        var theLink = ((photoUrl.match(linkLinter)[1]))
+        return theLink
     }
 
-    //Body selector case for images wrapped in 'a' tags
+    // Determines if last underscore in url before .jpg is part of path or just an image size modifier
+    function checkIfUnderscoreIsPath(imagePath) {
+       var underScoreFragment = imagePath.substring(imagePath.lastIndexOf("_") + 1, imagePath.lastIndexOf("."));
+
+        // Flickr uses convention _ +  a letter to resize images via url
+        if (underScoreFragment.length > 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    // Body selector case for images wrapped in 'a' tags
     $('body').on('mouseenter', '.overlay', function() {
         var $imgDiv = $(this).closest('.photo-list-photo-interaction');
         if ($imgDiv.find('.photon-button').length != 0) {
@@ -46,24 +61,49 @@ $(function(){
 
         $imgDiv.find('.photon-button').one('click', function() {
             var styleAttrib = $imgDiv.closest('.photo-list-photo-view').attr('style');
-            var imagePath = createPhotoUrl(styleAttrib);
+            var imagePath = (createPhotoUrl(styleAttrib).replace(/"/g, ""));
 
-            console.log('https:' + imagePath);
+            //If image path has more than one _, then this link.replace(/_.$/g, ""))
+            if (checkIfUnderscoreIsPath(imagePath)) {
+                imagePath = imagePath.replace(/\.jpg/g, "");
+            } else {
+                imagePath = imagePath.replace(/(_[a-z])(\.jpg+)$/g, "");
+            }
+
+            console.log('https:' + imagePath + '_b.jpg');
         })
     });
 
+    // This handles regular images on the website
+
+    // Restrict image size
+    const MAX_WIDTH = 100;
+    const MAX_HEIGHT = 100;
+
     var images = $('img');
 
-    $('img').on('mouseenter', function() {
-        var $imgDiv = $(this).closest('.photo_container');
-        if (!$imgDiv) {
-            console.log('test passed');
-            addZeButton($imgDiv);
-        }
-    });
+    // Adding a container div to make it easier to locate in the DOM
+    var outerDiv = $('<div>').addClass('outer');
 
-    $('img').on('mouseleave', function() {
-        $("button:contains('photon')").remove();
+    var hoverDiv = $('<div>').addClass('hover-div');
+    var addPhotoButton = $('<input>', {class: 'photon-button'}).attr({type: 'button', value: 'add photo'}).appendTo(hoverDiv);
+
+    // Only applies to images that are a certain size aka not thumb-nails
+    var mainImages = images.filter(function(i, image) { return (image.clientWidth > MAX_WIDTH && image.clientHeight > MAX_HEIGHT) });
+    mainImages.wrap(outerDiv);
+    $('.outer').append(hoverDiv);
+         
+    $('.hover-div').one('click', '.photon-button', function(evt) {
+        evt.preventDefault();
+        var imageLink = $(this).closest('.outer').children('img').attr('src');
+        imageLink = imageLink.replace(/"/g, "");
+
+        if (checkIfUnderscoreIsPath(imageLink)) {
+            imageLink = imageLink.replace(/\.jpg/g, "");
+        } else {
+            imageLink = imageLink.replace(/(_[a-z])(\.jpg+)$/g, "");
+        }
+        console.log(imageLink + '_b.jpg');
     });
 
 });
