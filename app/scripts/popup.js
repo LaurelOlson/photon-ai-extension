b28c5bcc1db28df6ac8b3055775d8d9177d017db
LaurelOlson <laurel.olson@outlook.com>
2016-03-29 21:44:48 -0700

    updated to allow local login

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -93,73 +93,45 @@ $(function() {
 
   // Laurel's stuff
 
-  // $('#login').submit(function (e) {
-  //   e.preventDefault();
-  //   console.log('hello world');
-  //   var email = $('#email').val();
-  //   var password = $('#password').val();
-  //   $.ajax ({
-  //     method: 'POST',
-  //     data: { email: email, password: password },
-  //     url: 'https://localhost:3000/login/ext',
-  //     dataType: 'json'
-  //   }).done(function(id) {
-  //     chrome.storage.sync.set({ 'user_id': id });
-  //   });
-  // });
+  $('#login').submit(function (e) {
+    e.preventDefault();
+    console.log('hello world');
+    var email = $('#email').val();
+    var password = $('#password').val();
+    $.ajax ({
+      method: 'POST',
+      data: { email: email, password: password },
+      url: 'https://localhost:3000/login/ext',
+      dataType: 'json'
+    }).done(function(id) {
+      chrome.storage.sync.set({ 'user_id': id });
+    });
+  });
 
-  // chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) { 
-  //   console.log('received message');
-  //   chrome.storage.sync.get(function(value) {
-  //     $.ajax({
-  //       method: 'POST',
-  //       data: { user_id: value['user_id'], url: req.url, width: req.width, height: req.height },
-  //       url: 'https://localhost:3000/addedphotos',
-  //       dataType: 'json'
-  //     }).done(function(res) {
-  //       console.log('send post req');
-  //       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //         chrome.tabs.sendMessage(tabs[0].id, { 'res': res });
+  chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) { 
+    console.log('received message');
+    chrome.storage.sync.get(function(value) {
+      $.ajax({
+        method: 'POST',
+        data: { user_id: value['user_id'], url: req.url, width: req.width, height: req.height },
+        url: 'https://localhost:3000/addedphotos',
+        dataType: 'json'
+      }).done(function(res) {
+        console.log('send post req');
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, { 'res': res });
 
-  // $('#login').submit(function (e) {
-  //   e.preventDefault();
-  //   var email = $('#email').val();
-  //   var password = $('#password').val();
-  //   $.ajax ({
-  //     method: 'POST',
-  //     data: { email: email, password: password },
-  //     url: 'https://localhost:3000/login/ext',
-  //     dataType: 'json'
-  //   }).done(function(id) {
-  //     chrome.storage.sync.set({ 'user_id': id });
-  //   });
-  // });
-
-  // $('#clickme').click(function (e) {
-    
-  //   e.preventDefault();
-
-  //   // Construct facebook OAuth url
-  //   var redirectUrl = chrome.identity.getRedirectURL();
-  //   // var redirectUrl = 'https://hijnoccjmdgleaafippfiocophahkhkl.chromiumapp.org/'
-  //   var clientId = "211877392505171";
-  //   var authUrl = "https://www.facebook.com/dialog/oauth?" +
-  //     "client_id=" + clientId + "&" +
-  //     "redirect_uri=" + encodeURIComponent(redirectUrl);
-
-  //   // Launch OAuth using Chrome Identity API
-  //   chrome.identity.launchWebAuthFlow({url: authUrl, interactive: true}, function(responseUrl) {
-  //     var accessToken = responseUrl.substring(responseUrl.indexOf("=") + 1);
-  //     console.log(accessToken);
-  //     $.ajax ({
-  //       method: 'POST',
-  //       data: { token: accessToken },
-  //       url: 'https://localhost:3000/login/ext/facebook',
-  //       dataType: 'json'
-  //     }).done(function(id) {
-  //       chrome.storage.sync.set({ 'user_id': id });
-
-  //     });
-  //   });
-
+  $('#login').submit(function (e) {
+    e.preventDefault();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    $.ajax ({
+      method: 'POST',
+      data: { email: email, password: password },
+      url: 'https://localhost:3000/login/ext',
+      dataType: 'json'
+    }).done(function(id) {
+      chrome.storage.sync.set({ 'user_id': id });
+    });
+  });
 
