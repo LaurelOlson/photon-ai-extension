6917c5462c1ef948472d09182a5c966ebe509e3b
Jedeu <jed.piezas@gmail.com>
2016-03-30 21:16:11 +0000

    Front page of 500px now works

--- app/embeds/photon-500px.js
+++ app/embeds/photon-500px.js
@@ -8,6 +8,60 @@ $(function() {
   //   var newButton = $imgLink.prepend($('<div>', {class: 'custom-icon-button'})); 
   // }
 
+  function checkICanHazHttps(imagePath) {
+    if (imagePath.match(/https/g) || imagePath.match(/http/g)) {
+      return imagePath;
+    }
+    else {
+      imagePath = 'https' + imagePath;
+      return imagePath;
+    }
+  }
+
+  function getNativeDimensions(imagePath, callback) {
+      var completePath = checkICanHazHttps(imagePath);
+      var output = {
+          url: completePath,
+          width: 0,
+          height: 0
+      };
+      var $img = $('<img>').attr({
+          src: completePath,
+          id: 'photonParseSizeTarget'
+      });
+      $img.addClass('make-invis');
+      $('body').append($img);
+      $img.on('load', function(){
+          var $zeImg = $(this);
+          output.width = $zeImg.width();
+          output.height = $zeImg.height();
+          $(this).remove();
+          callback(output);
+      });
+  }
+
+  function cleanUpStyle(styleAttrib) {
+      var parts = styleAttrib.split("; ");
+      var obj = {};
+      for (var i = 0; i < parts.length; i++) {
+        var subParts = parts[i].split(': ');
+        obj[subParts[0]]=subParts[1];
+      }
+      return obj;
+  }
+
+  function createPhotoUrl(styleAttrib) {
+      var cleanStyle = cleanUpStyle(styleAttrib);
+      var photoUrl = cleanStyle['background-image'];
+      var linkLinter = /\(([^\)]+)\)/;
+      var theLink = ((photoUrl.match(linkLinter)[1]));
+      return theLink;
+  }
+
+  function parseImg(imgObj) {
+      chrome.runtime.sendMessage({ url: imgObj.url, width: imgObj.width, height: imgObj.height });
+  }
+
   function addZeButton($imgLink) {
     $imgLink.prepend($('<div>', { class: 'flip' }));
     var $cardContainer = $imgLink.children('.flip');
@@ -16,27 +70,9 @@ $(function() {
     $cardContainer.children('.card').append($('<div>', { class: 'face back' }));
   }
 
-
-  // $('body').on('mouseenter', '.photo_link', function() {
-  //   var $imgLink = $(this).siblings('.photo_thumbnail__pulse_container');
-  //   if ($imgLink.find('.custom-icon-button').length != 0) {
-  //       return;
-  //   } else {
-  //       addZeButton($imgLink);
-  //   }
-
-  //   $imgLink.children('.custom-icon-button').one('click', function() {
-  //     // No idea why but couldn't traverse DOM the regular way...
-  //     var imgUrl = $imgLink.closest('.photo_link').context.firstElementChild.currentSrc;
-  //     console.log(imgUrl); 
-  //   });
-    
-  // });
-
-
   $('body').on('mouseenter', '.link', function() {
 
-    var $imgLink = $(this).closest('.photo.lazy-hidden');
+    var $imgLink = $(this).closest('.lazy-hidden');
     if ($imgLink.find('.flip').length === 0) {
       addZeButton($imgLink);
       $imgLink.find('.custom-icon-button').css({
@@ -63,6 +99,27 @@ $(function() {
       }
     });
 
+    $imgLink.find('.flip').one('click', function() {
+        var flipDiv = $(this);
+        var zeButtonFront = flipDiv.find('.face .front');
+        var zeButtonBack = flipDiv.find('.back');
+        var zeElem = flipDiv.closest('.lazy-hidden');
+        var styleAttrib = zeElem.attr('style');
+        var imagePath = (createPhotoUrl(styleAttrib)).replace(/"/g, "");
+
+        flipDiv.attr('data-clicked', 'true');
+
+        chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
+            if (req) {
+                zeButtonFront.css({'background-image': 'url("chrome-extension://hijnoccjmdgleaafippfiocophahkhkl/images/check-32.png")'});
+                zeButtonBack.css({'background-image': 'url("chrome-extension://hijnoccjmdgleaafippfiocophahkhkl/images/check-32.png")'});
+            }
+        });
+
+        getNativeDimensions(imagePath, parseImg);
+
+    });
+
   });
 
   $('body').on('mouseleave', '.photo.lazy-hidden', function() {
@@ -80,6 +137,22 @@ $(function() {
   //       'bottom': ($(this).height() - logoWidth + verticalDistanceFromWindow) + 'px'
   //     });
   //   }
+  // });
+
+    // $('body').on('mouseenter', '.photo_link', function() {
+  //   var $imgLink = $(this).siblings('.photo_thumbnail__pulse_container');
+  //   if ($imgLink.find('.custom-icon-button').length != 0) {
+  //       return;
+  //   } else {
+  //       addZeButton($imgLink);
+  //   }
+
+  //   $imgLink.children('.custom-icon-button').one('click', function() {
+  //     // No idea why but couldn't traverse DOM the regular way...
+  //     var imgUrl = $imgLink.closest('.photo_link').context.firstElementChild.currentSrc;
+  //     console.log(imgUrl); 
+  //   });
+    
   // });
 
 
